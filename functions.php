a057200d79b5e77fe8b19810b442c3f9310af856
akprinceinfo <akprince0268@gmail.com>
2024-08-23 22:37:01 +0600

    Logo Function Add

--- functions.php
+++ functions.php
@@ -23,5 +23,27 @@
     }
     add_action("wp_enqueue_scripts","themes_css_js_calling");
 
-?>
-<!-- ======  4 Start =============== -->
+
+    // Theme Function 
+
+    // Logo Add
+
+    function theme_cusomizer_regester($wp_customize){
+        $wp_customize->add_section('theme_header_area',array(
+            'title' => __('Header Area','themeTextDomain'),
+            'description'=> 'If you interested to update your header area, You can do it hear'
+        ));
+        $wp_customize->add_setting('theme_logo',array(
+            'default' => get_bloginfo('template_directory').'img/logo.jpg',
+        ));
+        $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize,'theme_logo',array(
+            'lable'=> 'Logo Upload',
+            'setting' => 'theme_logo',
+            'section' => 'theme_header_area',
+        )));
+    }
+    add_action('customize_register','theme_cusomizer_regester');
+
+
+
+    
--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 
 ========*/
 
+if (isset($_REQUEST['send'])) {
+    $name = $_REQUEST['name'];
+    $roll = $_REQUEST['roll'];
+
+    echo $name . $roll;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="<?php language_attributes();?>" class="no-js" >
@@ -15,6 +22,24 @@
 </head>
 <body <?php body_class(); ?>>
     
+    <!-- logo area  -->
+    <div id="hrader_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <!-- daynamick data -->
+                    <a href=""><img src="<?php echo get_theme_mod('theme_logo'); ?>" alt="" srcset=""></a>
+                    <!-- statect data  -->
+                    <!-- <a href=""><img src="<?#php echo get_template_directory_uri(); ?>/img/logo.jpg" alt="" srcset=""></a> -->
+                </div>
+                <div class="col-md-9"></div>
+            </div>
+        </div>
+    </div>
+
+   
+
+
     <?php wp_footer(); ?>
 </body>
 </html>
