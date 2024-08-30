65a5e096160b378de17e574f82572f020dd15cde
akprinceinfo <akprince0268@gmail.com>
2024-08-31 00:34:04 +0600

    Parmalink || title || the_excerpt ||

--- inc/default.php
+++ inc/default.php
@@ -2,3 +2,10 @@
 
 //Theme Title add
 add_theme_support('title-tag');
+
+
+// Thumbnail Image Are
+add_theme_support('post-thumbnails',array('page','post'));
+add_image_size('post-thumbnails',970,350,true);
+
+
--- index.php
+++ index.php
@@ -18,7 +18,15 @@
                             while (have_posts()) : the_post();
                         ?>
                     <div class="blog_area">
-                        <?php the_content(); ?>
+                        <div class="post_thumb">
+                            <a href="the_permalink();"><?php  echo the_post_thumbnail('post-thumbnails') ?></a>
+                        </div>
+                        <div class="post-detels">
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+                            <?php the_excerpt() ?>
+                        </div>
+                        <!-- যদি পোস্টের কন্টেন্টে ম্যানুয়ালি সারাংশ (excerpt) সেট করা না থাকে, তাহলে the_excerpt() ফাংশন স্বয়ংক্রিয়ভাবে পোস্টের প্রথম 55টি শব্দ গ্রহণ করে এবং তারপরে [...] যোগ করে। -->
+                        <?php # the_content(); ?>
                     </div>
                         <?php 
                             endwhile;
