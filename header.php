980b39340fad1b509f8d3fd6d98d4e8b2ffb6680
akprinceinfo <akprince0268@gmail.com>
2024-08-30 23:33:40 +0600

    Blog Post Add

--- header.php
+++ header.php
@@ -18,14 +18,35 @@
         
         
         <header id="header_area" class="<?php echo get_theme_mod('theme_Menu_position'); ?>">
-            
-        <header id="header_area">
-            <div id="notice">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p>Some Nothification</p>
-                    </div>
+        
+        <div class="container">
+            <div class="row">
+                <!-- logo area  -->
+                <div class="col-md-3 logo">
+                    <!-- daynamick data -->
+                    <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('theme_logo'); ?>" alt="" srcset=""></a>
+                    <!-- statect data  -->
+                    <!-- <a href=""><img src="<?#php echo get_template_directory_uri(); ?>/img/logo.jpg" alt="" srcset=""></a> -->
+                </div>
+                <!-- nav area  -->
+                <div class="col-md-9 <?php echo get_theme_mod('theme_color_setting'); ?>">
+                    <!-- daynamick menu add -->
+                    <?php wp_nav_menu(array('theme_location'=>'mani_menu','menu_id'=>'nav') ); ?>
+                    
+                    <!-- <ul id="nav">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Media</a></li>
+                        <li><a href="#">Setvice</a>
+                            <ul>
+                                <li><a href="#">Setvice 1</a></li>
+                                <li><a href="#">Setvice 2</a></li>
+                                <li><a href="#">Setvice 3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Contact</a></li>
+                    </ul> -->
                 </div>
             </div>
-        </header>
-        
+        </div>
+    </header>
